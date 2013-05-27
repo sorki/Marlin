245c12dfc89b19c1fca4d3911dab5f4628a9b34a
Richard Marko <rmarko@fedoraproject.org>
2013-05-27 17:25:37 +0200

    Y_HOME_DIR -> 1

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -250,7 +250,7 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 // ENDSTOP SETTINGS:
 // Sets direction of endstops when homing; 1=MAX, -1=MIN
 #define X_HOME_DIR -1
-#define Y_HOME_DIR -1
+#define Y_HOME_DIR 1
 #define Z_HOME_DIR -1
 
 #define min_software_endstops true //If true, axis won't move to coordinates less than HOME_POS.
