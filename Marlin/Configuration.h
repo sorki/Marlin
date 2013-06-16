29efe601957e2c059b1b985a475e4abba8b0736f
Miro Hrončok <miro@hroncok.cz>
2013-06-16 16:00:50 +0200

    Using values from FIT CTU green printer

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -282,9 +282,9 @@ const bool Z_ENDSTOPS_INVERTING = false; // set to true to invert the logic of t
 
 // default settings 
 
-#define DEFAULT_AXIS_STEPS_PER_UNIT   {60, 60, 2560, 760*1.1}
-#define DEFAULT_MAX_FEEDRATE          {500, 500, 2, 25}    // (mm/sec)
-#define DEFAULT_MAX_ACCELERATION      {9000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
+#define DEFAULT_AXIS_STEPS_PER_UNIT   {64,64,2560,633}
+#define DEFAULT_MAX_FEEDRATE          {250, 250, 5, 70}    // (mm/sec)
+#define DEFAULT_MAX_ACCELERATION      {6000,9000,100,10000}    // X, Y, Z, E maximum start speed for accelerated moves. E default values are good for skeinforge 40+, for older versions raise them a lot.
 
 #define DEFAULT_ACCELERATION          3000    // X, Y, Z and E max acceleration in mm/s^2 for printing moves 
 #define DEFAULT_RETRACT_ACCELERATION  3000   // X, Y, Z and E max acceleration in mm/s^2 for r retracts
